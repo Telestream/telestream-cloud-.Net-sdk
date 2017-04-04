cb0fabb9dbac93a0b1606f87ed467f1eee4d055d
Krzysztof Dziurda <krzysztofd@telestream.net>
2017-04-04 11:58:21 +0200

    Change parameters sorting order. Ignore null values returned in json.

--- Telestream.Cloud.SDK/Core/DataSerializer.cs
+++ Telestream.Cloud.SDK/Core/DataSerializer.cs
@@ -8,31 +8,34 @@ using Telestream.Cloud.SDK.Exceptions;
 
 namespace Telestream.Cloud.SDK.Core
 {
-	public class DataSerializer : IDataSerializer
-	{
-		public TResult Deserialize<TResult>(string value)
-		{
-			try
-			{
-				return JsonConvert.DeserializeObject<TResult>(value);
-			}
-			catch (JsonException ex)
-			{
-				throw new TelestreamCloudInvalidDataException(ex.Message, ex);
-			}
-		}
+        public class DataSerializer : IDataSerializer
+        {
+                public TResult Deserialize<TResult>(string value)
+                {
+                        try
+                        {
+                            var settings = new JsonSerializerSettings();
+                            settings.NullValueHandling = NullValueHandling.Ignore;
+
+                            return JsonConvert.DeserializeObject<TResult>(value, settings);
+                        }
+                        catch (JsonException ex)
+                        {
+                                throw new TelestreamCloudInvalidDataException(ex.Message, ex);
+                        }
+                }
 
 
-		public string Serialize(object data)
-		{
-			try
-			{
-				return JsonConvert.SerializeObject(data);
-			}
-			catch (JsonException ex)
-			{
-				throw new TelestreamCloudInvalidDataException(ex.Message, ex);
-			}
-		}
-	}
+                public string Serialize(object data)
+                {
+                        try
+                        {
+                                return JsonConvert.SerializeObject(data);
+                        }
+                        catch (JsonException ex)
+                        {
+                                throw new TelestreamCloudInvalidDataException(ex.Message, ex);
+                        }
+                }
+        }
 }
